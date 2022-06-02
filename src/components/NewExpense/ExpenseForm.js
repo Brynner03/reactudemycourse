91a790a5a76546cc368404f3f37d12d26e67488c
Brynner03 <bventura03@hotmail.com>
2022-06-02 15:51:14 -0400

    adding communication from child to parent

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 import '../../styles/ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
@@ -52,22 +52,25 @@ const ExpenseForm = () => {
                         amount: enteredAmount,
                         date: new Date(enteredDate)
                     }
-                    console.log(expenseData)
+                    props.onSaveExpenseData()
+                    setEnteredTitle('')
+                    setEnteredAmount('')
+                    setEnteredDate('')
                 }
   return (
     <form onSubmit={submitHandler} >
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2023-12-31' onChange={dayChangeHandler} />
+                <input type='date' min='2019-01-01' max='2023-12-31' value={enteredDate} onChange={dayChangeHandler} />
             </div>
         </div>
         <div className='new-expense__actions'>
