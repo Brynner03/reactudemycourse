c1c4b8c5a72abc6dce3a435eb8d03ee4eeaea53c
Brynner03 <bventura03@hotmail.com>
2022-06-02 14:44:08 -0400

    Working on useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,26 @@
+import React, { useState } from 'react'
 import '../../styles/ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
 
-    const handleClick = () => {
-        console.log('Cllicked!')
+    const [title, setTitle] = useState(props.title)
+
+    const clickHandler = () => {
+        setTitle('Updated!')
+        console.log(title)
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={handleClick} >Change Title</button>
-            {/* No Parentheses after handleClick because event will occur on load */}
+            <button onClick={clickHandler} >Change Title</button>
+            {/* No Parentheses after clickHandler because then event will occur on load */}
         </Card>
     )
 }
